9f802942b79cd54cf446642efbc00b356351828f
Carlos Monteiro <carlos.monteiro@ifc.edu.br>
2025-04-28 19:04:32 -0300

    funcionando, inclusive com LSTM

--- teste2.py
+++ teste2.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 
@@ -9,83 +10,70 @@ import matplotlib.pyplot as plt
 import math
 from sklearn.preprocessing import MinMaxScaler
 
+from organizar_pd import *
+
 #Google: pytorch for time series forecasting
 
 class RedeNeurotica(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers_lstm, dropout=0.2):
         super(RedeNeurotica, self).__init__()
+        #self.fc2 = nn.LSTM(hidden_size, hidden_size, num_layers_lstm, batch_first=True, dropout=dropout)
         self.fc1 = nn.Linear(input_size, hidden_size)
-        #self.fc2 = nn.LSTM(10, hidden_size, num_layers_lstm, batch_first=True, dropout=dropout)
         self.fc2 = nn.Linear(hidden_size,hidden_size)
         self.fc3 = nn.Linear(hidden_size, 2)
 
     def forward(self, x):
-        out = self.fc1(x.view(x.size(0), -1))
-        out = self.fc2(out)
+        out = F.relu(self.fc1(x.view(x.size(0), -1)))
+        #out, _ = self.fc2(out)
+        out = F.relu(self.fc2(out))
         out = self.fc3(out)
         return out       
 
 
 class AdmRede():
   def __init__(self, data_file, train_len=600, num_atrasos=1, en_ti=False, hidden_size=30, num_layers_lstm=3, dropout=0.2):
-    self.dados_pd = pd.read_csv(data_file)
-    self.dados_pd.drop(['Unnamed: 0'], axis=1, inplace=True)
-    self.dados_pd.drop(['time'], axis=1, inplace=True)
-    self.dados_pd.drop(['open'], axis=1, inplace=True)
-    self.dados_pd.drop(['high'], axis=1, inplace=True)
-    self.dados_pd.drop(['low'], axis=1, inplace=True)
-    self.dados_pd.drop(['real_volume'], axis=1, inplace=True)
+    self.dados_pd = organizar_pd(3, 1).get_dados()
 
-    if en_ti == False:
-      self.dados_pd.drop(['SMA'], axis=1, inplace=True)
-      self.dados_pd.drop(['WMA'], axis=1, inplace=True)
-      self.dados_pd.drop(['Momentum'], axis=1, inplace=True)
-      self.dados_pd.drop(['StochasticD'], axis=1, inplace=True)
-      self.dados_pd.drop(['StochasticK'], axis=1, inplace=True)
-      self.dados_pd.drop(['Williams'], axis=1, inplace=True)
-      self.dados_pd.drop(['RSI'], axis=1, inplace=True)
-      self.dados_pd.drop(['MACD'], axis=1, inplace=True)
-      self.dados_pd.drop(['ADO'], axis=1, inplace=True)
-      self.dados_pd.drop(['CCI'], axis=1, inplace=True)
-
-    self.dados_pd['direcao'] = self.dados_pd['direcao'].astype('int')
+    #print(f'pd {self.dados_pd.head(6)}')
 
     self.train_len = train_len
     self.test_len = len(self.dados_pd)-self.train_len
 
-    self.X_train = self.dados_pd[self.dados_pd.columns[0:self.dados_pd.shape[1]-1]].head(self.train_len)
-    self.y_train = self.dados_pd[self.dados_pd.columns[self.dados_pd.shape[1]-1]].head(self.train_len)
-    self.X_test = self.dados_pd[self.dados_pd.columns[0:self.dados_pd.shape[1]-1]].tail(self.test_len)
-    self.y_test = self.dados_pd[self.dados_pd.columns[self.dados_pd.shape[1]-1]].tail(self.test_len)
+    x, y = [], []
+    for _, row in self.dados_pd.iterrows():
+      row_values = row.to_list()
+      x.append([[val] for val in row_values[:-1]])
+      y.append([int(row_values[-1])])
 
-    self.X_train = np.array(self.X_train.values.tolist())
-    self.y_train = np.array(self.y_train.values.tolist()).reshape(len(self.y_train), 1)
-    self.X_test = np.array(self.X_test.values.tolist())
-    self.y_test = np.array(self.y_test.values.tolist()).reshape(len(self.y_test), 1)
+    #print(f'\n\n ------------ \nX\n{x[5]} \n\n ------------- {len(x)}')
+    self.X_train = x[:self.train_len]
+    self.y_train = y[:self.train_len]
+    self.X_test = x[self.train_len:]
+    self.y_test = y[self.train_len:]
 
-    # Create sequences and labels for training data
-    X_train, y_train = [], []
-    for i in range(len(self.X_train) - num_atrasos):
-      X_train.append(self.X_train[i:i + num_atrasos])
-      y_train.append(self.y_train[i + num_atrasos])
-    self.X_train, self.y_train = np.array(X_train), np.array(y_train)
+    self.X_train = np.array(self.X_train)
+    self.y_train = np.array(self.y_train).reshape(len(self.y_train), 1)
+    self.X_test = np.array(self.X_test)
+    self.y_test = np.array(self.y_test).reshape(len(self.y_test), 1)
+
+    #print(f'\nX Test\n {self.X_test[5]} \n\n ---------------- \n\n {self.X_test.shape}')
+    #print(f'\ny Test\n {self.y_test[5]} \n\n ---------------- \n\n {self.y_test.shape}')
+
+    
     self.X_train = torch.tensor(self.X_train, dtype=torch.float32)
     self.y_train = torch.tensor(self.y_train, dtype=torch.long).squeeze()
-
-    X_test, y_test = [], []
-    for i in range(len(self.X_test) - num_atrasos):
-      X_test.append(self.X_test[i:i + num_atrasos])
-      y_test.append(self.y_test[i + num_atrasos])
-    self.X_test, self.y_test = np.array(X_test), np.array(y_test)
     self.X_test = torch.tensor(self.X_test, dtype=torch.float32)
     self.y_test = torch.tensor(self.y_test, dtype=torch.long).squeeze()
 
-    #print(f'Len: {len(self.X_test[0])} - {self.X_test.shape}')
+    #print(f'\nX Test2\n {self.X_test[5]} \n\n ---------------- \n\n {self.X_test.shape}')
+    #print(f'\ny Test2\n {self.y_test[5]} \n\n ---------------- \n\n {self.y_test.shape}')
+
+
     self.modelo = RedeNeurotica(len(self.X_test[0]), hidden_size, num_layers_lstm, dropout)
 
-  def treinar(self, batch_size = 10, epochs=5, lr=0.001):
+  def treinar(self, batch_size = 50, epochs=10, lr=0.001, shuffle=True):
         train_dataset = TensorDataset(self.X_train, self.y_train)
-        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
 
         optimizer = torch.optim.Adam(self.modelo.parameters(), lr=lr)
         loss_fn = nn.CrossEntropyLoss()
